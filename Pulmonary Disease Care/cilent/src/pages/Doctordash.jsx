8219f58264b376482407954d08a871a49761c589
abhishekas7 <abhishekas2023a@mca.ajce.in>
2023-05-13 09:39:45 +0530

    hosting checking

--- Pulmonary Disease Care/cilent/src/pages/Doctordash.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Doctordash = () => {
-  return (
-    <div>Doctordash</div>
-  )
-}
-
-export default Doctordash
--- Pulmonary Disease Care/server/model/ProductModel.js
+++
@@ -1,13 +0,0 @@
-const mongoose = require(‘mongoose’);
-
- 
-
-at Ma ee eg
-Cesta
-Cesta
-password: String
-
-D3
-
-CNS oe a ae oy
-
