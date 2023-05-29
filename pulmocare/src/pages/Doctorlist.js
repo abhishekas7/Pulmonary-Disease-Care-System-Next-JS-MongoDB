e0c027809d7737977daa667c7df562e8a07876b8
Abhishek A S <94697425+abhishekas7@users.noreply.github.com>
2023-05-29 17:07:59 +0000

    updates on doctorlist

--- pulmocare/src/pages/Doctorlist.js
+++ pulmocare/src/pages/Doctorlist.js
@@ -73,19 +73,19 @@ export async function getServerSideProps() {
 
   // Find the document with the given ID in Document1
 
-  const doctor = await Doctor.find()
-    .populate({
-      path: "user",
-      match: { role: "doctor" },
-      select: "-_id -__v -password", // exclude these fields from the result
-    })
-    .lean();
-  // const product = await Product.findOne({ _id: params.productdetails});
-  // console.log(doctor);
- await db.disconnect();
-  return {
-    props: {
-      doctor: JSON.parse(JSON.stringify(doctor)),
-    },
-  };
-}
+//   const doctor = await Doctor.find()
+//     .populate({
+//       path: "user",
+//       match: { role: "doctor" },
+//       select: "-_id -__v -password", // exclude these fields from the result
+//     })
+//     .lean();
+//   // const product = await Product.findOne({ _id: params.productdetails});
+//   // console.log(doctor);
+//  await db.disconnect();
+//   return {
+//     props: {
+//       doctor: JSON.parse(JSON.stringify(doctor)),
+//     },
+//   };
+// }
